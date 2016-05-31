fa59806342ba868aed147da927e0f239d941bbc2
lczxhf <lczxhf@sina.com>
2016-05-31 17:39:25 +0800

    change config of redis and sidekiq and mysql

--- config/initializers/redis_object.rb
+++ config/initializers/redis_object.rb
@@ -1,2 +1,2 @@
 require 'connection_pool'
-Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new(:host => '127.0.0.1', :port => 6379,:db => 3,:driver => :hiredis) }
+Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new(:host => '172.17.0.1', :port => 36379,:db => 3,:driver => :hiredis) }
